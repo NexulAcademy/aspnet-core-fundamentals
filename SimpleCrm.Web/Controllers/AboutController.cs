32445bdfda494ffba48f417dda78659990e6cef1
Michael Lang <michael.lang@nexul.com>
2019-09-10 22:37:41 -0500

    Step011: mvc attribute routing

--- SimpleCrm.Web/Controllers/AboutController.cs
+++ SimpleCrm.Web/Controllers/AboutController.cs
@@ -1,11 +1,17 @@
-﻿namespace SimpleCrm.Web.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleCrm.Web.Controllers
 {
+    [Route("[controller]")]
     public class AboutController
     {
+        [Route("")]
+        [Route("[action]")]
         public string Phone()
         {
             return "555-555-1234";
         }
+        [Route("[action]")]
         public string Address()
         {
             return "USA";
