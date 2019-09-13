d0355cf45f7dd4e03db3c10efb609223fa8d61de
Michael Lang <michael.lang@nexul.com>
2019-09-12 20:57:32 -0500

    Step021: create SqlCustomerData and a DbContext

---
+++ SimpleCrm.SqlDbServices/SimpleCrmDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SimpleCrm.SqlDbServices
+{
+    public class SimpleCrmDbContext : DbContext
+    {
+        public SimpleCrmDbContext(DbContextOptions<SimpleCrmDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
