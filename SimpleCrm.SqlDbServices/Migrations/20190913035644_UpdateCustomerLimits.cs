c7d15087f9db7272c71b35546f0737a2709b7375
Michael Lang <michael.lang@nexul.com>
2019-09-12 23:01:56 -0500

    Step023: migrations added then altered

---
+++ SimpleCrm.SqlDbServices/Migrations/20190913035644_UpdateCustomerLimits.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SimpleCrm.SqlDbServices.Migrations
+{
+    public partial class UpdateCustomerLimits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Customers",
+                maxLength: 12,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Customers",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Customers",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Customers",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 12,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Customers",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Customers",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+        }
+    }
+}
---
+++ SimpleCrm.SqlDbServices/SimpleCrmDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System.Reflection;
+
+namespace SimpleCrm.SqlDbServices
+{
+    public class SimpleCrmDbContextFactory : IDesignTimeDbContextFactory<SimpleCrmDbContext>
+    {
+        public SimpleCrmDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<SimpleCrmDbContext>();
+            builder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SimpleCrm;Trusted_Connection=True;MultipleActiveResultSets=true",
+                optionsBuilder => optionsBuilder.MigrationsAssembly(typeof(SimpleCrmDbContext).GetTypeInfo().Assembly.GetName().Name));
+            return new SimpleCrmDbContext(builder.Options);
+        }
+    }
+}
