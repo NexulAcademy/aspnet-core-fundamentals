1c98dca0287eb0555d4e81187fdf404dd4f2ba7e
Michael Lang <michael.lang@nexul.com>
2019-09-14 22:43:48 -0500

    Step030: add and use a greeting view component

--- SimpleCrm.Web/Controllers/HomeController.cs
+++ SimpleCrm.Web/Controllers/HomeController.cs
@@ -7,22 +7,18 @@ namespace SimpleCrm.Web.Controllers
     public class HomeController : Controller
     {
         private ICustomerData _customerData;
-        private IGreeter _greeter; // <- new line
 
         public HomeController(
-            ICustomerData customerData,
-            IGreeter greeter
+            ICustomerData customerData
         )
         {
             _customerData = customerData;
-            _greeter = greeter;
         }
 
         public IActionResult Index()
         {
             var model = new HomePageViewModel();
             model.Customers = _customerData.GetAll();
-            model.CurrentMessage = _greeter.GetGreeting();
             return View(model);
         }
         public IActionResult Details(int id)
