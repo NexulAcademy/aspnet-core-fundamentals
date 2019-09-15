fcb4bcc437f32cd0009417813b0bdbe5a6c2a325
Michael Lang <michael.lang@nexul.com>
2019-09-15 09:38:05 -0500

    Step032: custom identity dbcontext & configuration

--- SimpleCrm.Web/Controllers/HomeController.cs
+++ SimpleCrm.Web/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace SimpleCrm.Web.Controllers
         )
         {
             _customerData = customerData;
+            
         }
 
         [AllowAnonymous]
--- SimpleCrm.Web/Startup.cs
+++ SimpleCrm.Web/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -28,9 +29,16 @@ namespace SimpleCrm.Web
             services.AddMvc();
             services.AddSingleton<IGreeter, ConfigurationGreeter>();
             services.AddScoped<ICustomerData, SqlCustomerData>();
+
             services.AddDbContext<SimpleCrmDbContext>(options =>
                 options.UseSqlServer(_configuration.GetConnectionString("SimpleCrmConnection"))
             );
+            services.AddDbContext<CrmIdentityDbContext>(options =>
+                options.UseSqlServer(_configuration.GetConnectionString("SimpleCrmConnection"))
+            );
+
+            services.AddIdentity<CrmUser, IdentityRole>()
+                .AddEntityFrameworkStores<CrmIdentityDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,9 +59,11 @@ namespace SimpleCrm.Web
                 });
             }
 
-            app.UseMvc(ConfigureRoutes);
-
             app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(ConfigureRoutes);
 
             app.Run(ctx => ctx.Response.WriteAsync("Not found, " 
                 + greeter.GetGreeting())); ;
