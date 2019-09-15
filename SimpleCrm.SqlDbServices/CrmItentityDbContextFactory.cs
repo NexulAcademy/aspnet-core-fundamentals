78d846eb59e6032a4a0b7ad78da1f2daa09ed14e
Michael Lang <michael.lang@nexul.com>
2019-09-15 10:07:40 -0500

    Step033: create an identity migration

---
+++ SimpleCrm.SqlDbServices/CrmItentityDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System.Reflection;
+
+namespace SimpleCrm.SqlDbServices
+{
+    public class CrmItentityDbContextFactory : IDesignTimeDbContextFactory<CrmIdentityDbContext>
+    {
+        public CrmIdentityDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<CrmIdentityDbContext>();
+            builder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SimpleCrm;Trusted_Connection=True;MultipleActiveResultSets=true",
+                optionsBuilder => optionsBuilder.MigrationsAssembly(typeof(CrmIdentityDbContext).GetTypeInfo().Assembly.GetName().Name));
+            return new CrmIdentityDbContext(builder.Options);
+        }
+    }
+}
